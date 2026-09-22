79f1ff754de4454c1ae175c0d6f79bc390bd7ea0
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-22 16:06:57 +0200

    ajout de la javadoc

--- src/Article.java
+++ src/Article.java
@@ -3,15 +3,35 @@ public class Article {
     private String brand;
     private Double price;
 
+
+    /**
+     * Constructeur de la classe Article.
+     *
+     * @param description la description de l'article
+     * @param brand       la marque de l'article
+     * @param price       le prix unitaire de l'article
+     */
     public Article(String description, String brand, Double price) {
         this.description = description;
         this.brand = brand;
         this.price = price;
     }
+
+/**
+     * Retourne une représentation textuelle de l'article.
+     *
+     * @return une chaîne décrivant l'article
+     */
+    @Override    
     public String toString(){
         return "l'article " + description + " a comme marque " + brand + " et coûte " + price + " euros.";
     }
 
+    /**
+     * Retourne le prix unitaire de l'article.
+     *
+     * @return le prix sous forme de Double
+     */
     public Double toDouble(){
         return price;
     }
