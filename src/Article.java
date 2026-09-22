b3561ff946d3d6b0ada7b0dbdc13f298184bff18
Damien Labbe <damien.labbe@fms-ea.com>
2026-09-22 14:33:43 +0200

    ajout du constructeur article et de la méthode toString

--- src/Article.java
+++ src/Article.java
@@ -1,3 +1,15 @@
 public class Article {
+    private String description;
+    private String brand;
+    private Double price;
+
+    public Article(String description, String brand, Double price) {
+        this.description = description;
+        this.brand = brand;
+        this.price = price;
+    }
+    public String toString(){
+        return "l'article " + description + " a comme marque " + brand + " et coûte " + price + " euros.";
+    }
     
 }
